48f238b3620164e3d64bb9fb6fe6329f1d17aca6
masterQian <292869319@qq.com>
2024-02-04 14:00:30 +0800

    Update Test.cpp

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -3,19 +3,9 @@
 #include <unordered_map>
 import MasterQian.Bin;
 import MasterQian.Data;
-import MasterQian.DB.Sqlite;
 import MasterQian.Log;
-import MasterQian.Media.Color;
-import MasterQian.Media.GDI;
-import MasterQian.Parser.Config;
-import MasterQian.Parser.Ini;
 import MasterQian.Storage.Path;
-import MasterQian.Storage.PDF;
-import MasterQian.Storage.Zip;
-import MasterQian.System;
 import MasterQian.Time;
-import MasterQian.Tool.Benchmark;
-import MasterQian.WinRT;
 using namespace MasterQian;
 
 #if 0
